ffcf98e18a3560694814fe6a84c04f4155ba83a6
Qian Wen Chee <qianwen.chee@gmail.com>
2025-08-10 10:58:53 +0800

    Update Instructions.py

--- pages/Instructions.py
+++ pages/Instructions.py
@@ -220,7 +220,7 @@ st.markdown("<div style='height: 58vh;'></div>", unsafe_allow_html=True)
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     if st.button("L E T' S\u00A0\u00A0G O!", use_container_width=True):
-        st.switch_page("pages/Instructions.py")
+        st.switch_page("pages/Modules.py")
 
 # Add empty content to prevent Streamlit from showing default content
 st.markdown("")

1f48db34b7970c4c1c8e253666a8a439d2976c3a
Qian Wen Chee <qianwen.chee@gmail.com>
2025-08-13 13:51:46 +0800

    Update Modules.py

--- pages/Modules.py
+++ pages/Modules.py
@@ -398,6 +398,7 @@ st.markdown("""
         font-family: 'Fredoka', cursive !important;
         font-weight: 700 !important;
         font-size: 1.1rem !important;
+        margin-top: -10px !important;
         border-radius: 30px !important;
         border: 3px solid rgba(255,255,255,0.8) !important;
         transition: all 0.4s cubic-bezier(0.68, -0.55, 0.265, 1.55) !important;

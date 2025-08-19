cda9ecd69f28f5844f8e63a7f507b8dffef12eab
Qian Wen Chee <qianwen.chee@gmail.com>
2025-08-19 21:50:36 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -174,7 +174,7 @@ page_styles = """
 
     /* Responsive spacing for button positioning */
     .button-spacing {
-        height: clamp(60vh, 62vh, 65vh) !important;
+        height: clamp(50vh, 55vh, 65vh) !important;
     }
 
     /* Force font loading for desktop only */

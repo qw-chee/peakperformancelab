767ff2207bfb4a326bf126b935bcdeaafe29c818
Qian Wen Chee <qianwen.chee@gmail.com>
2025-08-22 17:35:19 +0800

    Update Imagery.py

--- pages/Imagery.py
+++ pages/Imagery.py
@@ -1158,7 +1158,7 @@ elif st.session_state.current_step == 8:
     # Usage instructions
     st.markdown("""
     <div class="movie-container">
-        <h3 style="color: #FFD700; font-family: 'Sigmar', cursive; font-size: clamp(1.6rem, 2.3vw, 1.8em); text-align: center; margin-bottom: clamp(12px, 1.8vh, 15px);">
+        <h3 style="color: #FFD700; font-family: 'Sigmar', cursive; font-size: clamp(1.3rem, 2vw, 2em); text-align: center; margin-bottom: 0px;">
             🎯 How to Use Your Script
         </h3>
         <div class="movie-text">

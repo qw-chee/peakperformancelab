0ca5d87775f6c5a09efe43e6a20301331f2e7e46
Qian Wen Chee <qianwen.chee@gmail.com>
2025-09-18 11:13:21 +0800

    Update Imagery.py

--- pages/Imagery.py
+++ pages/Imagery.py
@@ -743,7 +743,6 @@ def get_movie_styles():
             font-size: clamp(1.2rem, 1.6vw, 1.2em) !important;
             font-family: 'Sigmar', cursive !important;
             width: fit-content !important;
-            margin: 10px auto !important;
             display: block !important;
         }
 
